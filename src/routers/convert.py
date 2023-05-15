18dd317f17c71dd3a0b59f540c2c94f050187126
haruboting <raiu_hk@icloud.com>
2023-05-16 03:32:16 +0900

    Change endpoint

--- src/routers/convert.py
+++ src/routers/convert.py
@@ -5,7 +5,7 @@ from src.interfaces.address import Address
 router = APIRouter()
 
 
-@router.post("/convert/divide_and_complete_address", tags=["cinvert"])
+@router.post("/convert/complete_and_divide_address", tags=["cinvert"])
 async def health(request: list[Address]):  # type: ignore
     completed_count: int = 0
     for address in request:
