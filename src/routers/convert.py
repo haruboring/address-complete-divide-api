01db5b0f4cd2c98746455416f9a6f27d386dfc98
haruboting <raiu_hk@icloud.com>
2023-05-15 15:26:38 +0900

    Delete extra comment and fix endpoint

--- src/routers/convert.py
+++ src/routers/convert.py
@@ -5,8 +5,7 @@ from src.interfaces.address import Address
 router = APIRouter()
 
 
-# health check
-@router.post("/convert/divide_address", tags=["cinvert"])
+@router.post("/convert/divide_and_complete_address", tags=["cinvert"])
 async def health(request: list[Address]):  # type: ignore
     for address in request:
         address.complete_address()
