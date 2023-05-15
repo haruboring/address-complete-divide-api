fca860f093bfb2839b69f01683b83921963a5637
haruboting <raiu_hk@icloud.com>
2023-05-16 00:04:02 +0900

    Add basic all function

--- src/routers/convert.py
+++ src/routers/convert.py
@@ -7,9 +7,12 @@ router = APIRouter()
 
 @router.post("/convert/divide_and_complete_address", tags=["cinvert"])
 async def health(request: list[Address]):  # type: ignore
+    completed_count: int = 0
     for address in request:
         address.complete_address()
+        address.normalize_address()
         address.divide_address()
+        completed_count = address.get_completed_address_count(completed_count)
         address.delete_extra_attributes()
 
-    return request
+    return {"completed_count": completed_count, "addresses": request}
