84697362db6b08c342f4ecb40fe05d7c72685079
haruboting <raiu_hk@icloud.com>
2023-05-16 00:14:38 +0900

    Implove function

--- src/functions/normalize.py
+++ src/functions/normalize.py
@@ -66,12 +66,14 @@ class Normalize:
     def convert_macron_to_hyphen(address: str) -> str:
         formatted_address: str = address
 
-        # 1ー43のような形は一つ目が番地と考えられるのでsearchでいい
-        match = re.search("([0-9]+ー)+[0-9]+", address)
-        if match is not None:
-            matched_string: str = match.group()
-            formatted_string: str = matched_string.replace("ー", "-")
-            formatted_address = address.replace(matched_string, formatted_string, 1)
+        # 1ー4－3のような形は一つ目が番地と考えられるのでsearchでいい
+        characters_similar_to_hyphen = ["－", "ー", "−"]
+        for character_similar_to_hyphen in characters_similar_to_hyphen:
+            match = re.search("([0-9]+" + character_similar_to_hyphen + ")+[0-9]+", address)
+            if match is not None:
+                matched_string: str = match.group()
+                formatted_string: str = matched_string.replace(character_similar_to_hyphen, "-")
+                formatted_address = address.replace(matched_string, formatted_string, 1)
 
         return formatted_address
 
